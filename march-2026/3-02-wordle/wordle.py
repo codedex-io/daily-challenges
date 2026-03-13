8c5fca8a65c5ded9def29123a6db16a3e8393df9
Sonny Li <sonnynomnom@gmail.com>
2026-03-13 05:25:16 -0400

    Refactor wordle.py to streamline guessing logic
    Removed previous game state comments and simplified the wordle_guess function.

--- march-2026/3-02-wordle/wordle.py
+++ march-2026/3-02-wordle/wordle.py
@@ -1,21 +1,11 @@
 # Codédex Daily Challenge #1
-# 6:12 4/5
-
-# 🟨🟨🟨🟨🟨
-# 🟨🟨🟨🟨🟨
-# 🟨🟨🟨🟨🟨
-# 🟩🟩🟩🟩🟩
-
-secret = "CODEX"
-guess = "COINS"
+# Wordle 🟩
 
 def wordle_guess(secret, guess):
-  # Write code below 💖
   count = 0
+  
   for i in range(5):
     if secret[i] == guess[i]:
       count = count + 1;
-  print(count)
+      
   return count
-
-wordle_guess(secret, guess)
