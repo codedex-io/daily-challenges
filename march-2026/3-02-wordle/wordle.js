242dde0178d9f213b452572db97dce0e25e20d32
aliya mahaboob <mahaboobaliya02@gmail.com>
2026-03-13 18:59:00 +0530

    Add JS solution for Day 1 challenge

---
+++ march-2026/3-02-wordle/wordle.js
@@ -0,0 +1,14 @@
+// Wordle 🟩
+// Codédex
+
+function wordleGuess(secret, guess) {
+
+  let count = 0;
+  for (let i = 0; i < secret.length; i++) {
+    if(secret[i] === guess[i]) {
+      count++
+    }
+  }
+  return count
+}
+console.log(wordleGuess("CODEX", "COINS"));
