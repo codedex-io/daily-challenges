9658dc7ba491c9c1aad97fd36641e67367096aad
Sonny Li <sonnynomnom@gmail.com>
2026-03-15 03:38:50 -0400

    Update world_wide_web.js

--- march-2026/3-12-world-wide-web/world_wide_web.js
+++ march-2026/3-12-world-wide-web/world_wide_web.js
@@ -7,12 +7,15 @@ function checkUrl(address) {
   };
   
   let domain = address.split("://");
+  
   domain = domain[1];
+  
   if (!domain.includes(".")) {
     return "invalid";
   };
   
   const allowedChars = /^[a-zA-Z0-9-.]+$/.test(domain);
+  
   if (!allowedChars) {
     return "invalid";
   }
