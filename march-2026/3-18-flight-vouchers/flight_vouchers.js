d4aca1c6bb51f21cdc05f46f15ad3b4313d8669b
Sonny Li <sonnynomnom@gmail.com>
2026-03-19 02:11:11 -0400

    Update flight_vouchers.js

--- march-2026/3-18-flight-vouchers/flight_vouchers.js
+++ march-2026/3-18-flight-vouchers/flight_vouchers.js
@@ -1,19 +1,19 @@
 // Flight Vouchers 🏖️ 
-// shercodes' solution
+// shercodes
 
 function pickVoucher(vouchers, delays, max_wait) {
-  // Sets the values for bestIndex and maxRatio.
-  let bestIndex = -1; // If none options qualify
-  let maxRatio = -1; // The highest ratio must be greater than -1
+  // Sets the values for bestIndex and maxRatio
+  let bestIndex = -1;   // If none options qualify
+  let maxRatio = -1;    // The highest ratio must be greater than -1
   
-  // Filters out any delay time that exceeds max_wait.
+  // Filters out any delay time that exceeds max_wait
   for (let i = 0; i < delays.length; i++) {
     if (delays[i] <= max_wait) { 
         
-      // Calculates the ratio of each valid voucher.
+      // Calculates the ratio of each valid voucher
       const ratio = vouchers[i] / delays[i];
 
-      // Finds the voucher with the highest ratio.
+      // Finds the voucher with the highest ratio
       if (ratio > maxRatio) {
         maxRatio = ratio;
         bestIndex = i;
