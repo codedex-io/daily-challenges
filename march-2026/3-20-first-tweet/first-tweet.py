359845687a4a0bd8a89dd44e469c63d7b243d162
alangnt <geirnaert.alan@icloud.com>
2026-03-21 18:57:09 +0100

    feat: first tweet python solution with explanations

---
+++ march-2026/3-20-first-tweet/first-tweet.py
@@ -0,0 +1,29 @@
+def tweet_balance(tweet):
+  # first we split the tweet and initialize the char count
+  split_tweet = tweet.split(" ")
+  char_count = 0
+
+  # for each word in the split tweet
+  for word in split_tweet:
+    # if @ add 20
+    if word.startswith("@"):
+      char_count += 20
+    # if url add 23
+    elif word.startswith("http://") or word.startswith("https://"):
+      char_count += 23
+    else:
+      # for each character
+      for char in word:
+        # if ascii value more than 0x1F000 it means it's an emoji
+        if ord(char) > 0x1F000:
+          char_count += 2
+        else:
+          char_count += 1
+
+    # space after each word
+    char_count += 1
+
+  # no trailing space after last word
+  char_count -= 1
+
+  return (140 - char_count)
